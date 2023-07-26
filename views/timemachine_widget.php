b0c3609e04bb1fce6c0fb0c69fdea1286a676024
tuxudo <tuxudo@users.noreply.github.com>
2023-07-26 12:30:34 -0400

    Move v6 Styling

--- views/timemachine_widget.php
+++ views/timemachine_widget.php
@@ -3,7 +3,7 @@
             <div class="card-header" data-container="body" title="">
                 <i class="fa fa-clock-o"></i>
                 <span data-i18n="timemachine.timemachine"></span>
-                <a href="/show/listing/timemachine/timemachine" class="pull-right text-reset"><i class="fa fa-list"></i></a>
+                <a href="/show/listing/timemachine/timemachine" class="pull-right"><i class="fa fa-list"></i></a>
             </div>
         <div class="card-body text-center"></div>
     </div><!-- /card -->
